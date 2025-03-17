60d755bfc5b0863814e152b3017c66c2a3528c65
Jessica Estes <jessica.m.estes@gmail.com>
2025-03-17 12:17:08 -0400

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Jessica Estes on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
